62a10b31b3e5cd48e88d8e5fd85a2d61debd90de
vinireys <vinireys10@gmail.com>
2023-08-23 13:45:17 -0300

    video 6

--- src/components/icons/CopyIcon.tsx
+++ src/components/icons/CopyIcon.tsx
@@ -1,6 +1,10 @@
 import { SVGProps } from "react";
 
-export const MenuIcon = (props: SVGProps<SVGSVGElement>) => {
+interface CopyInconProps extends SVGProps<SVGSVGElement> {
+  title?: string;
+}
+
+export const CopyIncon = (props: CopyInconProps) => {
   return (
     <svg
       {...props}
@@ -10,6 +14,7 @@ export const MenuIcon = (props: SVGProps<SVGSVGElement>) => {
       width='1em'
       xmlns="http://www.w3.org/2000/svg"
     >
+      {props.title && <title>{props.title}</title>}
       <path fill="none" d="M0 0h24v24H0V0z"></path>
       <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"></path>
     </svg>
