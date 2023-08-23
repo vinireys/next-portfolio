c91542963ae0ab4750357eea56405cc4658240de
vinireys <vinireys10@gmail.com>
2023-08-23 15:36:30 -0300

    video 6

--- src/components/commons/Header/index.tsx
+++ src/components/commons/Header/index.tsx
@@ -1,6 +1,9 @@
 import Image from "next/image";
 import Link from "next/link";
 import { Roboto } from 'next/font/google';
+import { Menu } from "./Menu";
+import { use, useCallback, useState } from "react";
+import { MenuIcon } from "../../icons/MenuIcon";
 
 const roboto = Roboto({
 	subsets: ['latin'],
@@ -8,19 +11,32 @@ const roboto = Roboto({
 });
 
 export const Header = () => {
+	const [isMenuOpen, setIsMenuOpen] = useState(false);
+  
+  const openMenu = useCallback(() => {
+		setIsMenuOpen(true)
+	}, [])
+
+	const closeMenu = useCallback( () => {
+		setIsMenuOpen(false)
+	}, []);
+
 	return(
 		<header 
-		 className={ `${roboto.className} bg-h-blue-900 text-sm flex py-3 px-5 justify-between items-center sticky top-0 z-20`}
+			className={ `${roboto.className} bg-h-blue-900 text-sm flex py-3 px-5 justify-between items-center sticky top-0 z-20`}
 		>
-				<Link href="/">
-					<Image src="/favicon.svg" width={55} height={55} alt="Icone da letra H, logo de Huriel" />			
-				</Link>
-				<nav className="hidden md:flex items-center gap-10 text-md">
-					<Link href="/">Sobre mim</Link>
-				    {/*<Link href="/">Portfolio</Link>*/}
-				   <Link href="/contatos">Entre em contato</Link>
+			<Link href="/">
+				<Image src="/favicon.svg" width={55} height={55} alt="Icone da letra H, logo de Huriel" />			
+			</Link>
+			<button className="p-1 md:hidden" onClick={openMenu}>
+				<MenuIcon className="fill-white w-10 h-10"/>
+			</button>
+			<nav className="hidden md:flex items-center gap-10 text-md">
+				<Link href="/">Sobre mim</Link>
+				{/*<Link href="/">Portfolio</Link>*/}
+				<Link href="/contatos">Entre em contato</Link>
 			</nav>
+			<Menu />
 		</header>
 	);
 };	 
-		 
