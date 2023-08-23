90cffb601ad9823454984c3dd683dbcacf7550b4
vinireys <vinireys10@gmail.com>
2023-08-23 14:15:30 -0300

    correçao do video 6

---
+++ src/components/commons/Header/Menu.tsx
@@ -0,0 +1,13 @@
+import Link from "next/link"
+
+export const Menu = () => {
+  return(
+    <div>
+      <div>
+        <div>
+          <Link href="/"></Link>
+        </div>
+      </div>
+    </div>
+  )
+}
