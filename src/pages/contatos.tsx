8402010bec55332d369861001c2c58b46ea57af3
vinireys <vinireys10@gmail.com>
2023-08-24 14:38:42 -0300

    correçao

--- src/pages/contatos.tsx
+++ src/pages/contatos.tsx
@@ -8,8 +8,8 @@ const Contatos =() => {
 			<title>Contatos  Huriel</title>
 			</Head>
 			<div className="mt-12 md:mt-24 space-y-8 md:space-y-16 px-6 md:px-32">
-			   <h1 className="text-5xl md:text-7xl font-bold text-center">Contatos</h1>
-			   <ul className="table mx-auto space-y-6 md:space-y-8">
+			  <h1 className="text-5xl md:text-7xl font-bold text-center">Contatos</h1>
+			  <ul className="table mx-auto space-y-6 md:space-y-8">
 					<li className="md:text-xl">
 						<span className="font-bold">E-mail</span>
 						<div className="flex gap-1 md:gap-3 items-center">
@@ -36,7 +36,7 @@ const Contatos =() => {
 					<li className="md:text-xl">
 						<span className="font-bold">GitHub</span>
 						<div className="flex gap-1 md:gap-3 items-center">
-						<a 	
+						 <a 	
 							  className="text-sm md:text-lg text-slate-300 underline truncate"
 						      href="https://www.linkedin.com/in/huri3l"
 							>
@@ -47,7 +47,7 @@ const Contatos =() => {
 					<li className="md:text-xl">
 						<span className="font-bold">YouTube</span>
 						<div className="flex gap-1 md:gap-3 items-center">
-						<a 	
+						  <a 	
 							  className="text-sm md:text-lg text-slate-300 underline truncate"
 						      href="https://www.linkedin.com/in/huri3l"
 							>
