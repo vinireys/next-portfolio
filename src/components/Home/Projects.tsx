b31606152cfbfeb6eb7c1f07014966965aba6102
vinireys <vinireys10@gmail.com>
2023-08-25 15:17:56 -0300

    video 6

--- src/components/Home/Projects.tsx
+++ src/components/Home/Projects.tsx
@@ -1,11 +1,29 @@
+import { Project } from "@/src/types/Home";
+import Image from "next/image";
+import Link from "next/link";
+
 interface ProjectProps {
-    projects: any;
+  projects: Project[];
 }
 
 export const Projects = ({ projects }: ProjectProps) => {
   return(
-    <div>
-      <h1>texto</h1>
-    </div>
+    <article>
+      <h2>Projetos Recentes</h2>
+
+      <ul>
+        {projects.map(({slug, name, image}, index) => (
+          <Link href={`/projects/${slug}`} key={name + index}>
+            <li>
+              <Image src={image.url} alt={image.alt} width={300} height={300} className=""/>
+              <span>{name}</span>
+              <div>
+                <span></span>
+              </div>
+            </li>
+          </Link>
+        ))}
+      </ul>
+    </article>
   );
 }; 
